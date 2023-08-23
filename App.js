b38cba2dda05c7b4d95b63a3d9d95103b81f05c1
AnuSandesh <sandesh.chandaliya@gmail.com>
2023-08-23 20:05:44 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React..!!"
+);
+
+const parnet = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Heading 1"),
+    React.createElement("h2", {}, "Heading 2"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+root.render(parnet);
+
+console.log(parnet);
